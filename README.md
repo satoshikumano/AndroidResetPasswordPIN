16bce657d3499cd1af48edaa140ee27409688549
satoshi <satoshi.kumano@kii.com>
2016-06-30 17:34:48 +0900

    Added README.

---
+++ README.md
@@ -0,0 +1,7 @@
+# AndroidResetPasswordPIN
+
+Test Application for Reset Password with PIN Code in receipt SMS
+
+## How to use
+- Go to developer.kii.com and create your app.
+- Edit Constants.java. and replace APP\_ID, APP\_KEY and APP\_URL with your application's. APP\_URL host would be "api.kii.com", "api-jp.kii.com", "api-sg.kii.com", "api-cn3.kii.com" and "api-eu.kii.com" depending on server location US, JP, SG, CN3 and EU.
--- app/src/main/java/example/com/kii/resetpasswordpin/Constants.java
+++ app/src/main/java/example/com/kii/resetpasswordpin/Constants.java
@@ -1,7 +1,7 @@
 package example.com.kii.resetpasswordpin;
 
 public class Constants {
-    public static final String APP_ID = "cd704e66";
-    public static final String APP_KEY = "cff7d016bc4da4da26807b71f720c84b";
-    public static final String APP_URL = "https://qa21.internal.kii.com/api";
+    public static final String APP_ID = "__appId";
+    public static final String APP_KEY = "__appKey";
+    public static final String APP_URL = "https://api-jp.kii.com/api";
 }
